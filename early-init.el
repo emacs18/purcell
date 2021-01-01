d52ef8bd310386fb42f695672101f3fa8df3904f
Richard Kim <emacs18@gmail.com>
2021-01-01 10:25:02 -0800

    setup to use straight.el rather than package.el

--- early-init.el
+++ early-init.el
@@ -10,6 +10,20 @@
 
 (setq package-enable-at-startup nil)
 
+(let* ((default-directory (file-name-directory load-file-name))
+       (init-file (expand-file-name "../init-straight.el")))
+  ;; Install packages under ./straight directory.
+  (setq straight-base-dir (expand-file-name ".local"))
+
+  ;; ../init-straight.el is the setup file that sets emacs up to use straight.el
+  ;; rather than pacakge.el. This file is in parent directory rather than
+  ;; current directory, because it is used for several other startup
+  ;; configurations.
+  (if (file-exists-p init-file)
+      (load init-file)
+    (error "Error: %s does not exist" init-file))
+  )
+
 ;; So we can detect this having been loaded
 (provide 'early-init)
 
